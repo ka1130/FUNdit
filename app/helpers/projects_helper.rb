d54c494b0a169a75bf7b6778ce2704a77a79db9b
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-12-04 23:22:16 +0100

    Add custom helper methods for formatting the data

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,9 @@
 module ProjectsHelper
+  def format_date(project)
+    if project.expired?
+      "#{distance_of_time_in_words_to_now(project.pledging_ends_on)} remaining"
+    else
+      content_tag(:strong, "All Done!")
+    end
+  end
 end
