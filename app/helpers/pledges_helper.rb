aab7cd7c4957428c511eed8502b6ea4e607f586c
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-01-11 23:38:35 +0100

    Add styles for the single project page / mobile view

--- app/helpers/pledges_helper.rb
+++ app/helpers/pledges_helper.rb
@@ -5,9 +5,9 @@ module PledgesHelper
     else
       amount_left = number_to_currency(project.amount_outstanding)
 
-      div = content_tag :div do
+      div = content_tag :div, class: 'pledge-cta-wrapper' do
         concat(content_tag(:p, "Only #{amount_left} left!"))
-        concat(link_to 'Pledge!', new_project_pledge_path(project))       
+        concat(link_to 'Pledge!', new_project_pledge_path(project), class: 'pledge-cta')       
       end
 
       return div
